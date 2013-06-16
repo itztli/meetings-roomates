24d29fb98101f792ad39ab11a0cd0e473d5a4b27
itztli <itztli@gmail.com>
2013-06-16 13:28:04 -0500

    testing commit

--- roomates/polls/models.py
+++ roomates/polls/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
+    #testing commit and push
     
     def __unicode__(self):
         return self.question
