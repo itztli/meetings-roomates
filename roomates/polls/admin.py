236fa04a3307288b9f99a99c94742e247b7336f5
itztli <itztli@gmail.com>
2013-06-20 19:56:58 -0500

    Configure Choice

--- roomates/polls/admin.py
+++ roomates/polls/admin.py
@@ -6,5 +6,19 @@ Created on Jun 16, 2013
 
 from django.contrib import admin
 from polls.models import Poll
+from polls.models import Choice
 
-admin.site.register(Poll)
+#admin.site.register(Poll)
+
+
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    
+    
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
+
--- roomates/polls/models.py
+++ roomates/polls/models.py
@@ -19,7 +19,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-
-    
     def __unicode__(self):
         return self.choice_text
