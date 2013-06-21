e729d9af21cfeb6ea42c58c5be6c541e04dcb01e
itztli <itztli@gmail.com>
2013-06-21 04:23:44 -0500

    testing commit

--- roomates/roomates/urls.py
+++ roomates/roomates/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
      url(r'^polls/', include('polls.urls', namespace="polls")),
      url(r'^admin/', include(admin.site.urls)),
+    
 )
 
 
